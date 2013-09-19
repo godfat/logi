d21aedff93513d02d099bb5ab8a9c2d514ed14e8
Lin Jen-Shin <godfat@godfat.org>
2013-09-20 00:27:52 +0800

    runner.rb: listing default values

--- lib/logi/runner.rb
+++ lib/logi/runner.rb
@@ -4,12 +4,20 @@ module Logi::Runner
   module_function
   def options
     @options ||=
-    [['-c, --color'      , '   Color mode (default)'],
+    [['logi options:'    , ''                       ],
+     ['-c, --color'      , '   Color mode (default)'],
      ['-n, --no-color'   , 'No color mode'          ],
      ['-V, --verbose'    , ' Verbose mode (default)'],
      ['-q, --quiet'      , '   Quiet mode'          ],
      ['-h, --help'       , 'Print this message'     ],
-     ['-v, --version'    , 'Print the version'      ]]
+     ['-v, --version'    , 'Print the version'      ],
+     ['defaults:'        , ''                       ],
+     ['  post directory' , Logi::Config.  default_post_path],
+     ['layout directory' , Logi::Config.default_layout_path],
+     ['output directory' , Logi::Config.default_output_path],
+     ['    post command' , Logi::Config.default_command    ],
+     ['     layout file' , Logi::Logger.strip_path(
+                             Logi::Config.default_layout)  ]]
   end
 
   def run argv=ARGV
@@ -59,11 +67,18 @@ module Logi::Runner
   end
 
   def help
-    maxn = options.transpose.first.map(&:size).max
-    maxd = options.transpose.last .map(&:size).max
+    require 'logi/config'
+    require 'logi/logger'
+    optt = options.transpose
+    maxn = optt.first.map(&:size).max
+    maxd = [optt.last .map(&:size).max, `tput cols`.to_i - maxn - 4].min
     "Usage: logi [OPTIONS] [PATH to logi root]\n" +
     options.map{ |(name, desc)|
-      sprintf("  %-*s  %-*s", maxn, name, maxd, desc)
+      if name.end_with?(':')
+        name
+      else
+        sprintf("  %-*s  %-*s", maxn, name, maxd, desc)
+      end
     }.join("\n")
   end
 end
