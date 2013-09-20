432fb47cda3fe7d1928646158d055ef4a1bda386
Lin Jen-Shin <godfat@godfat.org>
2013-09-20 15:19:26 +0800

    logi-post: error on post non-existing; warn on layout non-existing

--- lib/logi/command/post.rb
+++ lib/logi/command/post.rb
@@ -1,12 +1,34 @@
 
+require 'logi/default'
+require 'logi/logger'
+
 require 'tilt'
 
 class  Logi         ; end
 module Logi::Command; end
 module Logi::Command::Post
+  extend Logi::Logger
+
   module_function
   def run argv
-    path, layout = argv
+    path, layout = argv.map(&:dup)
+    check(path, layout)
     puts Tilt.new(layout).render{Tilt[path].new{$stdin.read}.render}
+  end
+
+  def check path, layout
+    unless File.exist?(path)
+      log red("ERROR: Post does not exist: #{cyan(strip_path(path))}")
+      exit(1)
+    end
+
+    unless File.exist?(layout)
+      log red("WARN: Layout does not exist: #{cyan(strip_path(layout))}")
+      layout.replace(Logi::Default.empty_layout)
+    end
+  end
+
+  def options
+    {:quiet => false}
   end
 end
