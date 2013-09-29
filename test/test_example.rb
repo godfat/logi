db05ee987b0bae233daa0d7337062af030b7045a
Lin Jen-Shin <godfat@godfat.org>
2013-09-29 18:54:35 +0800

    test against the log

--- test/test_example.rb
+++ test/test_example.rb
@@ -11,10 +11,24 @@ describe Logi do
   end
 
   should 'make' do
+    logs = []
+    stub(comp).log(is_a(String)){ |msg| logs << "#{msg}\n" }
+
     mock(comp).write(is_a(String), is_a(IO)){ |path, io|
       File.read(path).should.eq(io.read)
     }.times(conf.posts.size)
 
     logi.make
+
+    logs.join.gsub(/\e\[\d+m/, '').should.eq <<-MESSAGE
+logi-wiki ./example/post/hello.md |
+logi-post ./example/post/hello.md ./example/layout/another.html.erb
+logi-wiki ./example/post/list |
+logi-list ./example/post/list ./example/layout/list.html.erb
+logi-wiki ./example/post/index.md |
+logi-post ./example/post/index.md ./example/layout/default.html.erb
+logi-wiki ./example/post/misc/about.md |
+logi-post ./example/post/misc/about.md ./example/layout/default.html.erb
+    MESSAGE
   end
 end
