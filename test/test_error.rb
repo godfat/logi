bb4d3bc16b6120ae066c9a60e93e8dd0d3648eca
Lin Jen-Shin <godfat@godfat.org>
2013-09-30 05:07:17 +0800

    test for bad layout

--- test/test_error.rb
+++ test/test_error.rb
@@ -7,6 +7,7 @@ describe Logi do
   conf = logi.config
   comp = logi.compiler
   post = Logi::Post.new('post', 'post.md')
+  list = Logi::Post.new('list', 'list', :layout => 'bad')
 
   after do
     Muack.verify
@@ -22,5 +23,25 @@ describe Logi do
     $stderr.rewind
     $stderr.read.gsub(/\e\[\d+m/, '').should.eq \
       "ERROR: Post does not exist: ./post/post.md\n"
+  end
+
+  should 'warn if the layout does not exist' do
+    $stderr.reopen(Tempfile.new('stderr'), 'r+')
+    mock(conf).posts{ {'path' => list} }
+
+    logi.make
+    path = conf.output_path_for(list)
+    File.should.exist?(path)
+
+    $stderr.rewind
+    $stderr.read.gsub(/\e\[\d+m/, '').should.eq <<-MESSAGE
+WARN: Layout does not exist: ./layout/bad
+WARN: Fallback to the default: ./lib/logi/layout/default.html.erb
+logi-wiki ./post/list |
+logi-list ./post/list ./layout/bad
+> ./public/list.html
+    MESSAGE
+
+    File.delete(conf.output_path_for(list))
   end
 end
